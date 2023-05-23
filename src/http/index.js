a4069494a9c776b262d17e667c939d40c289cf28
zhangzzy <1546744276@qq.com>
2023-05-23 09:29:28 +0800

    34

--- src/http/index.js
+++ src/http/index.js
@@ -18,3 +18,16 @@ api.interceptors.request.use(
   }
 )
 
+// 添加响应拦截器
+api.interceptors.response.use(
+  (response) => {
+    // 对响应数据做一些处理
+    return response
+  },
+  (error) => {
+    // 处理响应错误
+    return Promise.reject(error)
+  }
+)
+
+export default api
